51160407ffcf6080fbb4cecf2dcda7b1cc02c101
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-15 01:22:35 +0530

    Add CategoryDTO class for category data transfer with JPA annotations

---
+++ src/main/java/com/example/stormgate_product_service/dto/CategoryDTO.java
@@ -0,0 +1,19 @@
+package com.example.stormgate_product_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryDTO implements Serializable {
+    private UUID id;
+    private String name;
+    private String description;
+}
